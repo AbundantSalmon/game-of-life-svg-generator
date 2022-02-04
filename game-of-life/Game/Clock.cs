cf44ab4205098e98b0c7b5881e9e9dc63b0ddcc2
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2022-02-04 21:46:06 +0800

    Codestyle change Clock

--- game-of-life/Game/Clock.cs
+++ game-of-life/Game/Clock.cs
@@ -12,40 +12,41 @@ namespace GameOfLife.Game
             // Prevent outside instantiation
         }
 
-        private bool _hasSetup = false;
+        private bool _hasBeenSetup = false;
         private int _totalTime; //seconds
         private int _timeBetweenTicks; //milliseconds
         private int _currentTime = 0; //milliseconds
 
         public void SetUp(int totalTime, int timeBetweenTicks)
         {
+            Debug.Assert(!_hasBeenSetup);
             _totalTime = totalTime;
             _timeBetweenTicks = timeBetweenTicks;
-            _hasSetup = true;
+            _hasBeenSetup = true;
         }
 
         public int getTotalTime()
         {
-            Debug.Assert(_hasSetup);
+            Debug.Assert(_hasBeenSetup);
             return _totalTime;
         }
 
         public bool Tick()
         {
-            Debug.Assert(_hasSetup);
+            Debug.Assert(_hasBeenSetup);
             _currentTime += _timeBetweenTicks;
             return (_currentTime < _totalTime * 1000);
         }
 
         public int getCurrentTime()
         {
-            Debug.Assert(_hasSetup);
+            Debug.Assert(_hasBeenSetup);
             return _currentTime;
         }
 
         public double getCurrentTimeFraction()
         {
-            Debug.Assert(_hasSetup);
+            Debug.Assert(_hasBeenSetup);
             return (double)_currentTime / ((double)_totalTime * 1000.0);
         }
     }
